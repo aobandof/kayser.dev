18d6ad900ab2ad2dbb2a2bbb1e37089168ecd7f8
ofaber82 <ofaber@outlook.com>
2017-08-16 00:05:23 -0300

    ok alert pero fala el confirm

--- sku/modal.js
+++ sku/modal.js
@@ -1,20 +1,38 @@
+
 $(document).ready(function() {
+  var confirmacion=false;
   $('#button_show_alert_saludo').click(function() {
     var param_modal={
       'id'            :   'div_modal_saludo',
-      'title'         :   'Saludo',
+      'title'         :   'Mensaje de Bienvenida',
       'message'       :   'Hola, Binvenido a esta pagina',
+      'type'          :   'alert'
     }
-    var modal_alert_saludo=show_modal_alert_confirm(param_modal);
+    showModalAlertConfirm(param_modal);
     // modal_alert_saludo.fadeIn('2000');
   })
+  $('#button_confirm').click(function() {
+    // console.log("entro a click confirm");
+    var param_modal={
+      'id'            :   'div_modal_confirm',
+      'title'         :   'Responda si o no',
+      'message'       :   'Esta seguro de avandonar esta pagina',
+      'type'          :   'confirm'
+    }
+    showModalAlertConfirm(param_modal);
+    console.log(confirmacion);
+    // if(confirmacion){
+    //   alert("tu respuesta fue si")
+    //   confirmacion=false;
+    // }
+    // alert(confirmarAccion())
+  });
 });
 
-function show_modal_alert_confirm(parametros){
+function showModalAlertConfirm(parametros){
   var cont_modal = document.createElement('div');
   cont_modal.id=parametros['id'];
   cont_modal.className="modal_alert_confirm";
-  // cont_modal.style.display='none';
   var modal=document.createElement('div');
   modal.className="content_modal";
   if(parametros['title']){
@@ -27,15 +45,34 @@ function show_modal_alert_confirm(parametros){
   body_modal.className="body_modal";
   body_modal.innerHTML="<p>"+parametros['message']+"</p>"
   modal.appendChild(body_modal);
+  var cont_close=document.createElement('div');
+  cont_close.className="close_modal";
+  if(parametros['type']==='alert'){
+    // var btn=document.createElement('button');
+    // cont_close.appendChild(crear_boton('OK',cont_close,cont_modal));
+    crear_boton('OK',cont_close,cont_modal);
 
-  // var cont_close=document.createElement('div');
-  // cont_close.className="close_modal"
-  // cont_close.appendChild(crear_boton('OK','close',));
-  // modal.appendChild(cont_close);
+  }else {
+    crear_boton('SI',cont_close,cont_modal,true);
+    crear_boton('NO',cont_close,cont_modal,false);
+    // cont_close.appendChild(crear_boton('SI',cont_modal,true));
+    // cont_close.appendChild(crear_boton('NO',cont_modal,false));
+  }
+  body_modal.appendChild(cont_close);
   cont_modal.appendChild(modal);
   document.body.appendChild(cont_modal);
-  return cont_modal;
 }
-function crear_boton(texto,tipo,id_contenedor){ //tipo = si, no, close, submit (close es para los alert y formularios)
-
+function crear_boton(texto,cont_añadir,contenedor_cerrar,retorno){ //tipo = si, no, close, submit (close es para los alert y formularios)
+  var btn=document.createElement('button');
+  btn.className="btn btn-success";
+  btn.innerHTML=texto;
+  var click= btn.addEventListener('click',function(){
+    document.body.removeChild(contenedor_cerrar);
+    if(retorno)
+      setearConfirmacion(retorno);s
+  });
+  cont_añadir.appendChild(btn);
+}
+function setearConfirmacion(valor){
+  confirmacion=valor;
 }
