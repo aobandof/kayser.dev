d8f9180d58ed13ed83a0ece08fa86c044d591775
Abel Rafael Obando Florián <aobandof@outlook.com>
2017-09-21 18:30:28 -0300

    2017-09-21-pm: cambios en stock vrweb y creaocion de BD kayser

--- script_ftp/stock_kayser_vrweb.php
+++ script_ftp/stock_kayser_vrweb.php
@@ -21,7 +21,7 @@ $conector_linea=$conexion_linea->obtener_conector();
 //   }
 // }
 $contenido="";
-$query_stock="select IdArticulo, CAST(SUM(Cantidad)-30 AS int) as Cant from Existencia where idAlmacen = '001' AND IdUbicacion LIKE '01%' GROUP BY IdArticulo HAVING SUM(Cantidad)>30 ORDER BY IdArticulo";
+$query_stock="select IdArticulo, CAST(SUM(Cantidad)-30 AS int) as Cant from Existencia where idAlmacen = '01' AND IdUbicacion LIKE '01%' GROUP BY IdArticulo HAVING SUM(Cantidad)>30 ORDER BY IdArticulo";
 $query_linea="select ItemCode from Kayser_OITM where U_tipoarticulo='L' ORDER BY ItemCode";
 $query_precio_detalle="SELECT  ItemCode, CAST(ROUND((Price*1.19),0) AS int) as DETALLE FROM Kayser_ITM1  where PriceList=15 AND ROUND((Price*1.19),0) IS NOT NULL and ROUND((Price*1.19),0)>1 ORDER BY ROUND((Price*1.19),0) ASC";
 $query_precio_promotoras="SELECT	ItemCode, CAST(ROUND((Price*1.19),0) AS int) AS PROMOTORAS FROM Kayser_ITM1  where PriceList=17 AND ROUND((Price*1.19),0) IS NOT NULL and ROUND((Price*1.19),0)>1 ORDER BY ROUND((Price*1.19),0) ASC";
@@ -56,7 +56,8 @@ if( $registros_linea === false ) {
 else {
   $file = fopen("plantilla_stock_kayser.csv", 'w'); //abrimos el archivo vacio o lleno, pero con "w" sobreescribimos
   //OBTENEMOS LOS NOMBRES DE CAMPOS Y LO CARGAMOS EN LA PRIMERA LINEA DEL ARCHIVO
-  $columna="IdArticulo;Cantidad;Precio Detalle;Precio Promotoras;\r\n";
+  /// $columna="IdArticulo;Cantidad;Precio Detalle;Precio Promotoras;\r\n";
+    $columna="IdArticulo;Cantidad;\r\n";///comentar esta linea cuando se agregue precio
   $cantidad=0;
   fwrite($file, $columna);
   $indice=-1;
@@ -65,14 +66,14 @@ else {
       $sku=$reg['ItemCode'];
       if(isset($arr_stock[$sku])){
         $fila=$fila.$sku.";".$arr_stock[$sku].";";
-        if(isset($arr_precio_detalle[$sku]))
+        /*if(isset($arr_precio_detalle[$sku]))
           $fila=$fila.$arr_precio_detalle[$sku].";";
         else
           $fila=$fila.";";# code...
         if(isset($arr_precio_promotoras[$sku]))
           $fila=$fila.$arr_precio_promotoras[$sku].";";
         else
-          $fila=$fila.";";# code...
+          $fila=$fila.";";# code...*/
         $fila=$fila."\r\n";
         fwrite($file, $fila);
       }
@@ -83,14 +84,14 @@ $ruta="stock"; // cuando la conexion sea con un usuario a quien se le asigno un 
 $hoy=date('d-m-Y H-m-s');
 $nombre_archivo_destino="stock_kayser.csv";//_".date('Y-m-d H-i-s').".csv";//date_format($hoy, 'Y-m-d H-m-s');
 /**** datos para conexion ftp ******/
-// $host="kayserps.vrserver7.cl";
-$host="200.72.246.165";
+$host="kayserps.vrserver7.cl";
+// $host="200.14.252.15";
 $port=21;
-// $user="kayserps";
-$user="ftpvrweb";
-// $password="ht658r9p62fb6s";
-$password="K4ys3rVrw3b235";
-// $ruta="/stock";
+$user="kayserps";
+// $user="ftpvrweb";
+$password="ht658r9p62fb6s";
+// $password="K4ys3rVrw3b235";
+$ruta="/stock";
 // $ruta="";
 /**** conexion *****/
 $conn_id=@ftp_connect($host,$port);
@@ -100,16 +101,16 @@ if($conn_id)
 		if(@ftp_login($conn_id,$user,$password))
 		{
 			# Cambiamos al directorio especificado
-			///if(@ftp_chdir($conn_id,$ruta))
-			///{
+			if(@ftp_chdir($conn_id,$ruta))
+			{
         ftp_pasv($conn_id, true);//parece ser que siempre hay errores, se agregó para se sea en modo pasivo
 				# Subimos el fichero
 				if(@ftp_put($conn_id,$nombre_archivo_destino,"plantilla_stock_kayser.csv",FTP_ASCII/*FTP_BINARY*/))
 					echo "Fichero subido correctamente";
 				else
 					echo "No ha sido posible subir el fichero";
-			///}else
-			  ///echo "No existe el directorio especificado";
+			}else
+			  echo "No existe el directorio especificado";
 		}else
 			echo "El usuario o la contraseña son incorrectos";
 		# Cerramos la conexion ftp
