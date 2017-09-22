22442693d59a3597ad3f55eba1ce9d7cd247e4bf
Abel Rafael Obando Florián <aobandof@outlook.com>
2017-09-22 13:51:00 -0300

    2017-09-22-pm: avances con npm y webpack

--- BDx/db_kayser_inserts_values.sql
+++ BDx/db_kayser_inserts_values.sql
@@ -5,12 +5,13 @@ insert into Area values (NULL,'INFORMATICA',''),(NULL,'DISEÑO',''),(NULL,'RECUR
 insert into Empleado values 
 (NULL,'24215707-9','OBANDO FLORIAN','ABEL RAFAEL','aobando@kayser.cl','949607845','DESARROLLADOR'),
 (NULL,'XXXXXXXX-X','MORA','MARIO','mmora@kayser.cl','xxxxxxxxxx','JEFE');
-/*select * from Area;
-select * from Empleado;*/
+select * from Area;
+select * from Empleado;
 insert into Ejecutivo values (1,1),(2,1);
 insert into Aplicacion values (NULL,'intranet_web');
 insert into User values
 (NULL,'aobando','123456','administrador',1),
 (NULL,'mmora','123456','administrador',2);
+select * from User;
 insert into Aplicacion_User values (1,1),(1,2);
 						
